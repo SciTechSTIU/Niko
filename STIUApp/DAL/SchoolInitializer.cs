3eb4e3158c2ec83a1dd5bc997e440f32943fd99b
lilmaayachu@hotmail.com <maaya.m@stamford.edu>
2015-01-22 18:52:20 +0700

    done

--- STIUApp/DAL/SchoolInitializer.cs
+++ STIUApp/DAL/SchoolInitializer.cs
@@ -47,8 +47,35 @@ namespace STIUApp.DAL
                 new Course{CourseID="LIB213", Title="Critical Thinking", Credits= 3, Faculty = "IT", Type="Basic Core"},
                 new Course{CourseID="LIB214", Title="Creative Thinking", Credits= 3, Faculty = "IT", Type="Basic Core"},
                 new Course{CourseID="SCI111", Title="Physic for Everyday life", Credits= 3, Faculty = "IT", Type="Basic Core"},
-                new Course{CourseID="ITE220", Title="Web Development 2", Credits= 3, Faculty = "IT", Type="Major Requisite"},
-
+                new Course{CourseID="ITE220", Title="Web Development 2", Credits= 3, Prereq="ITE120" ,Faculty = "IT", Type="Major Requisite"},
+                new Course{CourseID="ITE254", Title="Mobile UX", Credits= 3, Prereq="ITE221,ITE235", Faculty = "IT", Type="Major Requisite"}, 
+                new Course{CourseID="ITE321", Title="System Analysis,Design & Implementation ", Credits= 3, Prereq="ITE222", Faculty = "IT", Type="Major Requisite"},
+                new Course{CourseID="ITE331", Title="Multimedia Technology", Credits= 3, Prereq ="ITE220", Faculty = "IT", Type="Major Requisite"},
+                new Course{CourseID="ITE337", Title="Content Management Systems", Credits= 3, Prereq="ITE321" ,Faculty = "IT", Type="Major Requisite"},            
+                new Course{CourseID="ITE420", Title="Information Security", Credits= 3, Prereq="ITE475" ,Faculty = "IT", Type="Major Requisite"},
+                new Course{CourseID="ITE442", Title="Database Management System", Credits= 3, Prereq="ITE321" ,Faculty = "IT", Type="Major Requisite"},    
+                new Course{CourseID="ITE475", Title="Data Communication and Networking", Credits= 3, Prereq="ITE231" ,Faculty = "IT", Type="Major Requisite"},
+                new Course{CourseID="ITE479", Title="IT Planning and Project Management", Credits= 3, Prereq="120 Cr+" ,Faculty = "IT", Type="Major Requisite"},
+                new Course{CourseID="ITE299", Title="Service Desk Internship", Credits= 3, Prereq="ITE201, 45cr" ,Faculty = "IT", Type="Internship"},
+                new Course{CourseID="ITE499", Title="Professional Internship", Credits= 3, Prereq="Course Complete" ,Faculty = "IT", Type="Internship"},
+                new Course{CourseID="ITE240", Title="Principle of Operating System", Credits= 3, Prereq="ITE231" ,Faculty = "IT", Type="Major Elective"},
+                new Course{CourseID="ITE340", Title="E-commerce System & Infrastructure", Credits= 3, Prereq="BUS206" ,Faculty = "IT", Type="Major Elective"},
+                new Course{CourseID="ITE342", Title="Mobile Apps Development 1", Credits= 3, Prereq="ITE222" ,Faculty = "IT", Type="Major Electives"},
+                new Course{CourseID="ITE343", Title="Mobile Apps Development 2", Credits= 3, Prereq="ITE222" ,Faculty = "IT", Type="Major Electives"},
+                new Course{CourseID="ITE351", Title="IT Capacity Planning", Credits= 3, Prereq="231" ,Faculty = "IT",Type="Major Electives"},
+                new Course{CourseID="ITE353", Title="Mobile Commerce & Computing", Credits= 3, Prereq="ITE235,ITE475" ,Faculty = "IT", Type="Major Elective"},
+                new Course{CourseID="ITE390", Title="Business Intelligence Application", Credits= 3, Prereq="STA101, BUS206" ,Faculty = "IT", Type="Major Elective"},
+                new Course{CourseID="ITE443", Title="Data Warehouse Systems & Data Mining", Credits= 3, Prereq="ITE442" ,Faculty = "IT", Type="Elective"},
+                new Course{CourseID="ITE445", Title="Mobile Games Development", Credits= 3, Prereq="ITE220" ,Faculty = "IT", Type="Major Elective"},
+                new Course{CourseID="ITE448", Title="Current Topics in Mobile Computing", Credits= 3, Prereq="ITE235" ,Faculty = "IT", Type="Major Elective"},
+                new Course{CourseID="ITE449", Title="Current Topics in Infrastrucuture Management", Credits= 3, Prereq="ITE231" ,Faculty = "IT", Type="Major Elective"},            
+                new Course{CourseID="ITE451", Title="Cloud Computing", Credits= 3, Prereq="ITE442, ITE475" ,Faculty = "IT", Type="Major Elective"},
+                new Course{CourseID="ITE452", Title="Big Data", Credits= 3, Prereq="ITE442, ITE475" ,Faculty = "IT", Type="Major Elective"},
+                new Course{CourseID="ITE480", Title="Special Project", Credits= 3,Faculty = "IT", Type="Major Elective"},
+                new Course{CourseID="ITE485", Title="System and Network Administration", Credits= 3, Prereq="ITE475" ,Faculty = "IT", Type="Major Elective"},
+                            
+            
+            
             };
 
             courses.ForEach(s => context.Courses.Add(s));
